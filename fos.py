5e7cf9dbf631b67f3c8aa5fe47ffe5871754d2c2
fotisvar <49517652+fotisvar@users.noreply.github.com>
2020-10-16 09:37:54 +0300

    Update fos.py

--- fos.py
+++ fos.py
@@ -1,2 +1,12 @@
-#This is a python project
-print("Hi this is python in GitHub")
+#This is a python project about converting temperature
+
+temp=int(input("What is the temperature today? ")) #an den valeis to int() tha pareis string
+
+unit=input("(F)ahrenheit or (C)elcius? ")
+
+if unit.upper()=="F":
+    converted=((temp-32)*5)/9
+    print(f"Today is {converted} Celsius")
+else:
+    converted=(temp/5)*9+32
+    print(f"Today is {converted} Fehrenheit")
